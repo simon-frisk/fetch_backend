c716f318bdae33896bd40dcc53752746b61662cb
Simon Frisk <simon@simonfrisk.com>
2023-09-30 12:41:40 -0500

    Add error handling and json parsing

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const logger = require("morgan")
+
 const router = require("./routes/router")
 
 const app = express()
@@ -7,9 +8,21 @@ const PORT = 8000
 
 // Configure middleware
 app.use(logger("dev"))
+app.use(express.json())
 
 // Router configuration
 app.use('/', router)
+
+// Send 404 if no routes match
+app.use((req, res, next) => {
+  res.status(404).send()
+})
+
+// Send 500 if an error happens
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Internal Server Error')
+})
 
 // Start server and listen to PORT
 app.listen(PORT, error => {
